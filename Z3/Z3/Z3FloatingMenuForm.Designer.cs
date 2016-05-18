95d2db884f32ffb6696a46d7c62e7deb3cd9a2bd
Daniel2218 <daniel_lucia22@hotmail.com>
2016-05-18 15:00:35 -0400

    Increase Menu Form Height

--- Z3/Z3/Z3FloatingMenuForm.Designer.cs
+++ Z3/Z3/Z3FloatingMenuForm.Designer.cs
@@ -30,10 +30,11 @@ namespace Z3.View.Floating {
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(414, 25);
+            this.ClientSize = new System.Drawing.Size(419, 34);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(100, 65);
             this.Name = "Z3FloatingMenuForm";
             this.Text = "Z3";
             this.ResumeLayout(false);
